7dd40df70baca7ee7dd4547ff02fee95df144826
JorgeJasso <JassoJorgeAlberto@gmail.com>
2019-02-19 20:22:09 -0600

    Practica 1
    Practica expuesta en clase (Hola mundo)

---
+++ Practica1HolaMundo/app/src/main/java/com/example/practica1holamundo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.practica1holamundo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_main);
+    }
+
+
+}
